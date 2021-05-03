13cb122d8cc617897a2983f778478c231d80a3d1
YUNA <jya3161@gmail.com>
2021-05-03 21:48:15 +0900

    Mimic kinectfusion2 (https://github.com/xijunke/KinectFusion-2)

--- tsdf-fusion-python-master/demo.py
+++ tsdf-fusion-python-master/demo.py
@@ -38,6 +38,7 @@ if __name__ == "__main__":
   print("Initializing voxel volume...")
   tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.02)
 
+
   # Loop through RGB-D images and fuse them together
   t0_elapse = time.time()
   for i in range(n_imgs):
--- tsdf-fusion-python-master/kinect_realtime_demo.py
+++ tsdf-fusion-python-master/kinect_realtime_demo.py
@@ -2,7 +2,6 @@
 """
 
 import time
-import json
 
 import cv2
 import numpy as np
@@ -11,8 +10,10 @@ import fusion
 
 # Kinect module
 import pyk4a
-from helpers import colorize
 from pyk4a import Config, PyK4A
+
+from icp_modules.ICP import ICP_point_to_point
+from icp_modules.FramePreprocessing import DepthMap,PointCloud
 
 if __name__ == "__main__":
   # ======================================================================================================== #
@@ -43,7 +44,6 @@ if __name__ == "__main__":
   t0_elapse = time.time() # for check fps
   iter = 0
   while True:
-    iter = iter+1
     # ======================================================================================================== #
     # (Optional) This is an example of how to compute the 3D bounds
     # in world coordinates of the convex hull of all camera view
@@ -51,14 +51,38 @@ if __name__ == "__main__":
     # ======================================================================================================== #
     capture = k4a.get_capture()
     if capture.depth is not None:
+      iter = iter + 1
       # Read depth image and camera pose
       #cv2.imshow("Depth", colorize(capture.depth, (None, 5000)))
-      depth_im = colorize(capture.depth, (None, 5000)).astype(float)
+      depth_im = capture.depth.astype(float)
       depth_im /= 1000.  ## depth is saved in 16-bit PNG in millimeters
       depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset) 65.535=2^16/1000
 
       # KinectFusion에서 pose estimation하는 것을 참고하여 연산
-      cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % (iter))  # 4x4 rigid transformation matrix
+
+      # Set first frame as world system
+      if iter == 1:
+        first_Depthmap = DepthMap(depth_im)
+        first_Points3D = PointCloud(first_Depthmap, np.linalg.inv(cam_intr))
+        continue
+      elif iter == 2:
+        second_Depthmap = DepthMap(depth_im)
+        second_Points3D = PointCloud(second_Depthmap, np.linalg.inv(cam_intr))
+        # ICP find Trans between neighboring frames
+        first_pose = ICP_point_to_point(first_Points3D, second_Points3D)
+        first_Points3D = second_Points3D
+        continue
+      else:
+        second_Depthmap = DepthMap(depth_im)
+        second_Points3D = PointCloud(second_Depthmap, np.linalg.inv(cam_intr))
+        # ICP find Trans between neighboring frames
+        pose = ICP_point_to_point(first_Points3D, second_Points3D)
+        cam_pose = np.dot(first_pose, pose)
+        first_Points3D = second_Points3D
+        first_pose = cam_pose
+
+        #cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % (iter))  # 4x4 rigid transformation matrix
+
 
       # Compute camera view frustum and extend convex hull
       view_frust_pts = fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
@@ -71,19 +95,19 @@ if __name__ == "__main__":
       # ======================================================================================================== #
       # Initialize voxel volume
       print("Initializing voxel volume...")
-      tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.02)
+      if iter == 3:
+        tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.01)
+      else:
+        tsdf_vol.set_vol_bnds(vol_bnds)
 
       # Loop through RGB-D images and fuse them together
       print("Fusing frame")
 
       # Read RGB-D image and camera pose
       color_image = cv2.cvtColor(capture.color,cv2.COLOR_BGR2RGB)
-      depth_im = colorize(capture.depth, (None, 5000)).astype(float)
+      depth_im =capture.depth.astype(float)
       depth_im /= 1000.  ## depth is saved in 16-bit PNG in millimeters
       depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset) 65.535=2^16/1000
-
-      # KinectFusion에서 pose estimation하는 것을 참고하여 연산
-      cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % (iter))  # 4x4 rigid transformation matrix
 
       # Integrate observation into voxel volume (assume color aligned with depth)
       tsdf_vol.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
@@ -102,11 +126,11 @@ if __name__ == "__main__":
   print("Average FPS: {:.2f}".format(fps))
 
   # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
-  print("Saving mesh to mesh.ply...")
+  print("Saving mesh to test_mesh.ply...")
   verts, faces, norms, colors = tsdf_vol.get_mesh()
   fusion.meshwrite("test_mesh.ply", verts, faces, norms, colors)
 
   # Get point cloud from voxel volume and save to disk (can be viewed with Meshlab)
-  print("Saving point cloud to pc.ply...")
+  print("Saving point cloud to test_pcd.ply...")
   point_cloud = tsdf_vol.get_point_cloud()
   fusion.pcwrite("test_pcd.ply", point_cloud)
