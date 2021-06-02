3c118367cd9dd822fa062a3f5b5035d626f51930
KIM MINSU <minsumunsu@gmail.com>
2021-06-03 00:40:58 +0900

    no message

--- tsdf-fusion-python-master/kinect_n_detectron2.py
+++ tsdf-fusion-python-master/kinect_n_detectron2.py
@@ -43,9 +43,70 @@ def filter_human(output):
     x, y = np.nonzero(1 - mask.numpy())
     return x, y
 
+def filter_joint(output):
+    joints = output["instances"].pred_keypoints[0][:, :2].numpy()
+    return joints
+
+
+def Joint_model():
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.DEVICE = 'cpu'
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    return predictor
+
+
+def joint_to_3D(joints, Inverse, pose, depth_im):
+    Joints = np.zeros((3, 17))
+    for i in range(17):
+        xx, yy = joints[i]
+        d = depth_im[int(round(yy)), int(round(xx))]
+        U = d * np.dot(Inverse, np.array([xx, yy, 1]).T)
+        Joints[:, i] = np.dot(pose, np.array([U[0], U[1], U[2], 1]))[:-1]
+    return Joints
+
+
+def simple_bundle(joint_3D: list):
+    # Outlier 날리고 평균 구하기.
+    joints_3D = np.array(joint_3D)  # N * 3 * 17
+    if len(joint_3D) > 50:
+        joint_val = [[0, 0, 0] for _ in range(17)]
+        counts = [[0.1, 0.1, 0.1] for _ in range(17)]
+        mean = np.mean(joints_3D, axis=0)  # 3x17
+        std = np.std(joints_3D, axis=0)  # 3x17
+        thr_low = mean + 2 * std
+        thr_high = mean - 2 * std
+        for i in range(len(joints_3D)):
+            for j in range(17):
+                if thr_low[0, j] < joints_3D[i, 0, j] < thr_high[0, j]:
+                    joint_val[j][0] += joints_3D[i, 0, j]
+                    counts[j][0] += 1
+                if thr_low[1, j] < joints_3D[i, 1, j] < thr_high[1, j]:
+                    joint_val[j][1] += joints_3D[i, 1, j]
+                    counts[j][1] += 1
+                if thr_low[2, j] < joints_3D[i, 2, j] < thr_high[2, j]:
+                    joint_val[j][2] += joints_3D[i, 2, j]
+                    counts[j][2] += 1
+        result = np.zeros((3, 17))
+        for i, (val, count) in enumerate(zip(joint_val, counts)):
+            xv = val[0]
+            xc = count[0]
+            yv = val[1]
+            yc = count[1]
+            zv = val[2]
+            zc = count[2]
+            result[:, i] = xv / xc, yv / yc, zv / zc
+
+    else:
+        result = np.mean(joints_3D, axis=0)
+    return result
+
 
 if __name__ == "__main__":
     model = SEG_model()
+    joint_model = Joint_model()
 
     # Open kinect camera by realtime
     k4a = PyK4A(
@@ -59,7 +120,7 @@ if __name__ == "__main__":
     # Load video file
     # filename = r'C:\Users\82106\PycharmProjects\dino_lib\python_kinect_fusion\video1.mkv'
     filename = r'C:\Users\82106\PycharmProjects\dino_lib\python_kinect_fusion\tsdf-fusion-python-master\human6.mkv'
-    n_frames = 4
+    n_frames = 30
 
 
     k4a = PyK4APlayback(filename)
@@ -67,7 +128,7 @@ if __name__ == "__main__":
 
     # Load Kinect's intrinsic parameter
     cam_intr = k4a.calibration.get_camera_matrix(pyk4a.calibration.CalibrationType.COLOR)
-
+    invK = np.linalg.inv(cam_intr)
     # List 생성
     list_depth_im = []
     list_color_im = []
@@ -91,12 +152,12 @@ if __name__ == "__main__":
             list_color_im.append(color_im)
 
             if iter == 0:
-                first_Points3D = PointCloud(depth_im, np.linalg.inv(cam_intr))
+                first_Points3D = PointCloud(depth_im, invK)
                 cam_pose = np.eye(4)
                 first_pose = cam_pose
 
             elif iter >= 1:
-                second_Points3D = PointCloud(depth_im, np.linalg.inv(cam_intr))
+                second_Points3D = PointCloud(depth_im, invK)
                 ind = random.sample(range(first_Points3D.shape[1]), second_Points3D.shape[1])
                 pose, distances, _ = icp(second_Points3D.T,
                                          first_Points3D.T[ind, :])  # A, B // maps A onto B : B = pose*A
@@ -121,6 +182,7 @@ if __name__ == "__main__":
     # ===============Integrate===============
     n_imgs = len(list_depth_im)
     iter = 0
+    joints_3D = []
     for iter in range(0, n_imgs):
         print("Fusing frame %d/%d" % (iter + 1, n_imgs))
 
@@ -137,21 +199,22 @@ if __name__ == "__main__":
         threshold = np.mean(depth_im[val_x, val_y]) + 2 * np.std(depth_im[val_x, val_y])
         depth_im[depth_im >= threshold] = 0
 
+
         # Set first frame as world system
         if iter == 0:
-            previous_Points3D = PointCloud(depth_im, np.linalg.inv(cam_intr))
+            previous_Points3D = PointCloud(depth_im, invK)
             cam_pose = np.eye(4)
             previous_pose = cam_pose
 
         elif iter == 1:
-            second_Points3D = PointCloud(depth_im, np.linalg.inv(cam_intr))
+            second_Points3D = PointCloud(depth_im, invK)
             pose, distances, _ = icp(second_Points3D.T, previous_Points3D.T)  # A, B // maps A onto B : B = pose*A
             cam_pose = np.dot(previous_pose, pose)
             previous_pose = cam_pose
             previous_Points3D = second_Points3D
 
         elif iter > 1:
-            Points3D = PointCloud(depth_im, np.linalg.inv(cam_intr))
+            Points3D = PointCloud(depth_im, invK)
 
             # Compute camera view frustum and extend convex hull
             pose, distances, _ = icp(Points3D.T, previous_Points3D.T)  # A, B // maps A onto B : B = pose*A
@@ -176,6 +239,11 @@ if __name__ == "__main__":
             cam_pose = pose
             previous_pose = cam_pose
             previous_Points3D = Points3D
+
+            output = joint_model(color_im)
+            joint = filter_joint(output)
+            joints_3D.append(joint_to_3D(joint, invK, pose, depth_im))
+
         poses.append(previous_pose)
         # Integrate observation into voxel volume (assume color aligned with depth)
         tsdf_vol.integrate(color_im, depth_im, cam_intr, cam_pose, obs_weight=1.)
@@ -189,6 +257,20 @@ if __name__ == "__main__":
     #
     #     pose = poses[i]
     #     human_vol.integrate(color_im, depth_im, cam_intr, pose, obs_weight=1.)
+    joint_ = simple_bundle(joints_3D)
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(projection='3d')  # Axe3D object
+    print(joint_.shape)
+    print(joint_)
+
+    joint_info = ['nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear', 'left_shoulder', 'right_shoulder',
+                  'left_elbow', 'right_elbow', 'left_wrist', 'right_wrist', 'left_hip', 'right_hip',
+                  'left_knee', 'right_knee', 'left_ankle', 'right_ankle']
+
+    for i in range(17):
+        ax.scatter(joint_[0, i], joint_[1, i], joint_[2, i]) # projection  P = 4XN
+        ax.text(joint_[0, i], joint_[1, i], joint_[2, i], joint_info[i], fontsize=10)
+    plt.show()
 
     # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
     print("Saving mesh")
